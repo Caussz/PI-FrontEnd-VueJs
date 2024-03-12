f181821eb6f7d3e58da5a2bf477677f62b80c296
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-03-12 15:15:16 -0300

    FEAT: Iniciando página de carregamento #4

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/zyra',
+      name: 'Zyra',
+      component: () => import('../views/ZyraView.vue')
     }
   ]
 })
---
+++ src/views/ZyraView.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <span>zyra.</span>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: #1E1E1E;
+    justify-content: center;
+    align-items: center;
+}
+
+span {
+    
+    color: #99B898;
+    rotate:270deg;
+    font-family: "Kodchasan", sans-serif;
+    font-size: 6rem;
+    font-weight: 400;
+
+}
+span::before, span::after{
+    content: "";
+    display: block;
+    height: 0;
+    width: 0;
+}
+span::before{
+    margin-top: -3rem;
+}
+</style>
