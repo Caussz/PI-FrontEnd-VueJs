6efb6b0fb667779dc6aea758281ab7c8d4809122
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-03-27 14:38:47 -0300

    FEAT: criação de arquivos para a responsividade.

---
+++ src/components/Login/LoginDesktop.vue
@@ -0,0 +1,17 @@
+<script setup>
+import FormLogin from '@/components/Login/FormLogin.vue'
+
+</script>
+
+<template>
+    <div class="form-container">
+    <FormLogin />
+    </div>
+</template>
+
+<style scoped>
+.form-container{
+    width: 30%;
+
+}
+</style>
