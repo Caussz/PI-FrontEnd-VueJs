4611aec4b97ddc4dc115bb5845c4e57c9813eda9
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-03-27 14:38:47 -0300

    FEAT: criação de arquivos para a responsividade.

---
+++ src/components/Login/FormLogin.vue
@@ -0,0 +1,111 @@
+<script setup>
+import { ref } from 'vue'
+
+import {
+    CheckCircle,
+    EyeOffOutline,
+    EyeOutline
+} from '@/components/icons'
+
+const verSenha = ref(false)
+</script>
+
+<template>
+    <form>
+        <h1>Entrar</h1>
+        <label>
+            <p>Usuário</p>
+            <div class="input-container">
+                <input type="email">
+                <CheckCircle />
+            </div>
+        </label>
+        <label>
+            <p>Senha</p>
+            <div class="input-container">
+                <input type="email">
+                <EyeOutline v-if="!verSenha" @click="verSenha = !verSenha" />
+                <EyeOffOutline v-if="verSenha" @click="verSenha = !verSenha" />
+            </div>
+        </label>
+        <button>Entrar</button>
+    </form>
+</template>
+
+<style scoped>
+label {
+    width: 100%;
+}
+
+.input-container {
+    width: 100%;
+    background-color: #3a3a3ab5;
+    border: 1px solid #505050;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    color: #716F6F;
+    padding: 5% 0;
+    border-radius: 9px;
+    margin-top: 1%;
+}
+.input-container:focus-within{
+    border: solid 1px #99b89873;
+}
+.input-container span {
+    height: 20px;
+}
+
+input {
+    background-color: transparent;
+    color: #716F6F;
+}
+
+input:focus-visible {
+    outline: 0;
+}
+
+button {
+    margin-top: 15%;
+    background-color: #99B898;
+    color: #000000;
+    font-weight: 500;
+    width: 100%;
+    padding-block: 1rem;
+    border-radius: 5rem;
+    font-size: 1.1rem;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 85%;
+    gap: 2vh;
+}
+
+main {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: #1E1E1E;
+    justify-content: center;
+    align-items: center;
+}
+
+p {
+    font-weight: 300;
+    font-size: .8rem;
+}
+
+h1 {
+    margin-bottom: 10%;
+    font-weight: 500;
+}
+
+h1,
+p {
+    color: #FFFFFF;
+}
+</style>
