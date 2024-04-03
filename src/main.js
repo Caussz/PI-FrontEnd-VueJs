77bcc48d7add3ccaf6448ef4933a7783e3fb2117
Caussz <cczcaua@gmail.com>
2024-04-02 21:27:44 -0300

    FEAT: Adicionando configuracoes de exemplos das rotas #9

--- src/main.js
+++ src/main.js
@@ -11,4 +11,17 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+const user = {
+    loggedIn: false
+};
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.meta.requiresAuth;
+    if (requiresAuth && !user.loggedIn) {
+      next('/zyra')
+    } else {
+      next()
+    }
+  })
+
 app.mount('#app')
