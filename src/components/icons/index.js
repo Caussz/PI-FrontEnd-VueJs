59566e1c2dd99f34ce3b7d507334616a74a2d9f4
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-04-17 18:42:10 -0300

    REFACTOR: Importação de icon para o header

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,9 +1,11 @@
 import CheckCircle from 'vue-material-design-icons/CheckCircle.vue';
 import EyeOffOutline from 'vue-material-design-icons/EyeOffOutline.vue';
 import EyeOutline from 'vue-material-design-icons/EyeOutline.vue';
+import Menu from 'vue-material-design-icons/Menu.vue';
 
 export {
     CheckCircle,
     EyeOffOutline,
-    EyeOutline
+    EyeOutline,
+    Menu
 }
