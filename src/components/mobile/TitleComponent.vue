198f3ee407346c28bda72bb32ae48b4f883ab51e
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-04-17 19:18:40 -0300

    FEAT: Criação do componente de título

---
+++ src/components/mobile/TitleComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+    const props = defineProps({
+        text: String
+    })
+</script>
+
+
+<template>
+    <h1>
+    {{ props.text }}
+    </h1>
+</template>
+
+<style scoped>
+h1{
+    text-align: center;
+    font-size: 1rem;
+    color: #FFFFFF;
+}
+</style>
