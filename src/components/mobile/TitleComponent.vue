3e75b69b8fb871c7f94d74614cad92edb3c40c6b
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-04-18 10:03:08 -0300

    REFACTOR: alterando componente de titulo para maior reutilização

--- src/components/mobile/TitleComponent.vue
+++ src/components/mobile/TitleComponent.vue
@@ -9,11 +9,6 @@ const props = defineProps({
 
 <template>
     <div class="container">
-        <span v-if="outHome">
-            <router-link to="/">
-                <ChevronLeft />
-            </router-link>
-        </span>
         <h1>
             {{ props.text }}
         </h1>
@@ -30,22 +25,16 @@ const props = defineProps({
 .container {
     position: relative;
     color: #FFFFFF;
-    margin: 7% 0 2% 0;
+    margin-top: 5%;
 }
 
-span {
+span{
     position: absolute;
     left: 5%;
 }
 
 h1 {
+    text-align: center;
     font-size: 1rem;
-}
-
-span {
-    display: flex;
-    color: #525252;
-    font-size: .8rem;
-    align-items: center;
 }
 </style>
