bddcc47d18fa0075ecf2849783d9afb5ea005e5a
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-08-20 15:02:02 -0300

    FEAT: ajuste em css e criação de back button

--- src/views/InfoUserView.vue
+++ src/views/InfoUserView.vue
@@ -1,63 +1,86 @@
-<script setup></script>
+<script setup>
+import {
+  ChevronLeft,
+  CameraOutline
+} from '@/components/icons'
+</script>
 <template>
-  <div class="user-image">
-    <div class="image">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/6326/6326055.png"
-      alt=""
-    />
-
+  <main>
+    <div class="top">
+      <div class="back-button">
+        <ChevronLeft class="icon" />
+      </div>
+      <div class="user-image">
+        <div class="image">
+          <img src="https://cdn-icons-png.flaticon.com/512/6326/6326055.png" alt="" />
+          <div class="camera">
+            <CameraOutline class="icon" size="15px" />
+          </div>
+        </div>
+        <p class="name">Oliver Calenbard</p>
+        <p class="id">2022305863</p>
+      </div>
     </div>
-    <p class="name">Oliver Calenbard</p>
-    <p class="id">2022305863</p>
-  </div>
-  <div class="main">
-    <div class="info">
-      <p>Nome</p>
-      <input type="text" />
+    <div class="container">
+      <div class="info">
+        <p>Nome</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Email</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Matrícula</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Curso</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Nível</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Status</p>
+        <input type="text" />
+      </div>
+      <div class="info">
+        <p>Entrada</p>
+        <input type="text" />
+      </div>
     </div>
-    <div class="info">
-      <p>Email</p>
-      <input type="text" />
-    </div>
-    <div class="info">
-      <p>Matrícula</p>
-      <input type="text" />
-    </div>
-    <div class="info">
-      <p>Curso</p>
-      <input type="text" />
-    </div>
-    <div class="info">
-      <p>Nível</p>
-      <input type="text" />
-    </div>
-    <div class="info">
-      <p>Status</p>
-      <input type="text" />
-    </div>
-    <div class="info">
-      <p>Entrada</p>
-      <input type="text" />
-    </div>
-  </div>
+  </main>
 </template>
 <style scoped>
-.main {
-  display: grid;
-  grid-template-columns: 25vw 25vw;
-  background-color: black;
+main {
+  background-color: #1e1e1e;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 27vw 25vw;
   margin: 2vw 0;
 }
 
 input {
-  background-color: #282828;
+  background-color: #3a3a3ab5;
   border-radius: 7px;
   width: 20vw;
   color: white;
   padding: 0.7vw 1.2vw;
+  border: 1px #505050 solid;
+}
+
+input:focus {
+  outline: none;
 }
 
 .info {
@@ -67,32 +90,56 @@ input {
   font-weight: 600;
 }
 
-.image {
+.user-image {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & .name {
+    font-weight: 600;
+  }
+
+  & .image {
+    & img {
+      width: 7vw;
+    }
+  }
+
+  & .id {
+    font-size: 0.8em;
+  }
 }
 
-.user-image {
-    background-color: grey;
-    color: white;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+.top {
+  width: 100vw;
+}
 
-    & .name {
-        font-weight: 600;
-        font-size: 1.1em;
-    }
+.back-button {
+  width: 2vw;
+  height: 2vw;
+  margin-left: 2vw;
+  border-radius: 10vw;
+  border: #505050 solid 1px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.3vw;
+}
 
-    & .image {
+.icon {
+  margin: 0.3vw 0.3vw 0 0.2vw; /* remover  */
+}
 
-        & img {
-            width: 7vw;
-        }
-    }
-
-    & .id {
-        font-size: 0.9em;
-    }
-
+.camera {
+  background-color: #181818;
+  border-radius: 2vw;
+  border: #505050 solid 1px;
+  width: 1.5vw;
+  height: 1.5vw;
+  position: relative;
+  left: 4vw;
+  bottom: 2vw
 }
 </style>
