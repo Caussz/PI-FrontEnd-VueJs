1e08b5ffcd831e30fc5b999f3bfb976afe099988
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-08-20 15:02:02 -0300

    FEAT: ajuste em css e criação de back button

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,9 +1,13 @@
 import CheckCircle from 'vue-material-design-icons/CheckCircle.vue';
 import EyeOffOutline from 'vue-material-design-icons/EyeOffOutline.vue';
 import EyeOutline from 'vue-material-design-icons/EyeOutline.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import CameraOutline from 'vue-material-design-icons/CameraOutline.vue';
 
 export {
     CheckCircle,
     EyeOffOutline,
-    EyeOutline
+    EyeOutline,
+    ChevronLeft,
+    CameraOutline
 }
