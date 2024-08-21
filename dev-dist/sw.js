efbc64c5247b4e267a231d74958d005859cfc198
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-08-21 15:06:22 -0300

    FEAT: mudança dos inputs para divs, ajustes no css das divs, ajustes no posicionamento dos elementos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aqm6kfqi6og"
+    "revision": "0.sm29evqbaf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/views/InfoUserView.vue
+++ src/views/InfoUserView.vue
@@ -14,7 +14,7 @@ import {
         <div class="image">
           <img src="https://cdn-icons-png.flaticon.com/512/6326/6326055.png" alt="" />
           <div class="camera">
-            <CameraOutline class="icon" size="15px" />
+            <CameraOutline class="icon" size="23" />
           </div>
         </div>
         <p class="name">Oliver Calenbard</p>
@@ -24,37 +24,39 @@ import {
     <div class="container">
       <div class="info">
         <p>Nome</p>
-        <input type="text" />
+        <div>Oliver Calenbard</div>
       </div>
       <div class="info">
         <p>Email</p>
-        <input type="text" />
+        <div>olivercalenbard2info1@gmail.com</div>
       </div>
       <div class="info">
         <p>Matrícula</p>
-        <input type="text" />
+        <div>2022305863</div>
       </div>
       <div class="info">
         <p>Curso</p>
-        <input type="text" />
+        <div>Informártica para internet</div>
       </div>
       <div class="info">
         <p>Nível</p>
-        <input type="text" />
+        <div>Técnico integrado</div>
       </div>
       <div class="info">
         <p>Status</p>
-        <input type="text" />
+        <div>Ativo</div>
       </div>
       <div class="info">
         <p>Entrada</p>
-        <input type="text" />
+        <div>Técnico integrado</div>
       </div>
     </div>
   </main>
 </template>
 <style scoped>
 main {
+  /* background-image: url(https://ibb.co/gVWNCXM); */
+  background-repeat: no-repeat;
   background-color: #1e1e1e;
   width: 100vw;
   height: 100vh;
@@ -66,17 +68,19 @@ main {
 
 .container {
   display: grid;
-  grid-template-columns: 27vw 25vw;
+  grid-template-columns: 27vw 27vw;
   margin: 2vw 0;
 }
 
-input {
-  background-color: #3a3a3ab5;
+.info div {
+  background-color: #282828;
   border-radius: 7px;
-  width: 20vw;
+  width: 22vw;
   color: white;
   padding: 0.7vw 1.2vw;
   border: 1px #505050 solid;
+  font-weight: 400;
+  margin: 0.2vw;
 }
 
 input:focus {
@@ -95,6 +99,7 @@ input:focus {
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 20vw;
 
   & .name {
     font-weight: 600;
@@ -113,6 +118,8 @@ input:focus {
 
 .top {
   width: 100vw;
+  display: grid;
+  grid-template-columns: 40vw 1vw;
 }
 
 .back-button {
