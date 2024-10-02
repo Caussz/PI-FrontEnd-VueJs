1cf1fe21fb68ccdeaea7b250b51785da1550f129
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-10-02 15:01:48 -0300

    FEAT: Ajustando funcionalidades nos popups

--- src/components/PopUp/PopUpFinishComponent.vue
+++ src/components/PopUp/PopUpFinishComponent.vue
@@ -1,63 +1,66 @@
 <script setup>
 import { Check } from "@/components/icons";
+import { useUserStore } from "@/stores";
+
+const { user } = useUserStore();
 </script>
 <template>
-  <div class="background-blur">
-    <div class="modal">
-      <Check class="icon" size="90" />
-      <p>Seu cadastro foi concluido com sucesso!</p>
-      <button>Prosseguir</button>
-    </div>
-  </div>
+	<div class="background-blur">
+		<div class="modal">
+			<Check class="icon" size="90" />
+			<p>Seu cadastro foi concluido com sucesso!</p>
+			<button @click="user.loggedStatus = 3">Prosseguir</button>
+		</div>
+	</div>
 </template>
 <style scoped>
 .background-blur {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  backdrop-filter: blur(3px);
-  background-color: rgba(0, 0, 0, 0.205);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 100;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	backdrop-filter: blur(3px);
+	background-color: rgba(0, 0, 0, 0.205);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
 }
 
 .modal {
-  position: absolute;
-  background-color: #282828;
-  width: 65vw;
-  padding: 10vw 8vw 7vw 8vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 7vw;
-  border-radius: 35px;
-  box-shadow: 0px 4px 30px 0px;
+	position: absolute;
+	background-color: #282828;
+	width: 65vw;
+	padding: 10vw 8vw 7vw 8vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 7vw;
+	border-radius: 35px;
+	box-shadow: 0px 4px 30px 0px;
 }
 
 .icon {
-  background: linear-gradient(180deg, #445244 0%, #99b898 100%);
-  padding: 4vw 5vw;
-  border-radius: 50%;
+	background: linear-gradient(180deg, #445244 0%, #99b898 100%);
+	padding: 4vw 5vw;
+	border-radius: 50%;
 }
 
 p {
-  color: white;
-  font-weight: 500;
-  font-size: 0.9rem;
-  display: flex;
-  text-align: center;
+	color: white;
+	font-weight: 500;
+	font-size: 0.9rem;
+	display: flex;
+	text-align: center;
 }
 
 button {
-  background-color: #99b898;
-  color: #000000;
-  font-weight: 500;
-  width: 100%;
-  padding-block: 1rem;
-  border-radius: 5rem;
-  font-size: 1.1rem;
+	background-color: #99b898;
+	color: #000000;
+	font-weight: 500;
+	width: 100%;
+	padding-block: 1rem;
+	border-radius: 5rem;
+	font-size: 1.1rem;
 }
 </style>
--- src/components/PopUp/PopUpLoginComponent.vue
+++ src/components/PopUp/PopUpLoginComponent.vue
@@ -1,75 +1,79 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from "@/stores";
+
+const { user } = useUserStore();
+</script>
 <template>
-  <div class="background-blur">
-    <div class="modal">
-      <h2>Cadastro</h2>
-      <p>
-        Para concluir o cadastro, é necessário inserir seu número de telefone no campo indicado e
-        clicar em <span style="color: #99b898;">"Enviar"</span>.
-      </p>
-      <div class="input-container">
-        <p>Telefone</p>
-        <input type="tel" placeholder="554712345678" />
-      </div>
-      <button>Enviar</button>
-    </div>
-  </div>
+	<div class="background-blur">
+		<div class="modal">
+			<h2>Cadastro</h2>
+			<p>
+				Para concluir o cadastro, é necessário inserir seu número de telefone no campo
+				indicado e clicar em <span style="color: #99b898">"Enviar"</span>.
+			</p>
+			<div class="input-container">
+				<p>Telefone</p>
+				<input type="tel" placeholder="554712345678" />
+			</div>
+			<button @click="user.loggedStatus = 2">Enviar</button>
+		</div>
+	</div>
 </template>
 <style scoped>
 .background-blur {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  backdrop-filter: blur(3px);
-  background-color: rgba(0, 0, 0, 0.205);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 100;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	backdrop-filter: blur(3px);
+	background-color: rgba(0, 0, 0, 0.205);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
 }
 
 .modal {
-  position: absolute;
-  background-color: #282828;
-  width: 65vw;
-  padding: 10vw 8vw 7vw 8vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 7vw;
-  border-radius: 35px;
-  box-shadow: 0px 4px 30px 0px;
+	position: absolute;
+	background-color: #282828;
+	width: 65vw;
+	padding: 10vw 8vw 7vw 8vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 7vw;
+	border-radius: 35px;
+	box-shadow: 0px 4px 30px 0px;
 }
 
 h2 {
-  color: #99b898;
-  font-weight: 500;
+	color: #99b898;
+	font-weight: 500;
 }
 
 p {
-  color: white;
-  font-weight: 500;
-  font-size: .76rem;
+	color: white;
+	font-weight: 500;
+	font-size: 0.76rem;
 }
 
 input {
-    background-color: #3A3A3AB5;
-    border: #99B898 1px solid;
-    outline: none;
-    width: 60vw;
-    padding: 2vh 2.3vw;
-    border-radius: 5px;
-    color: #716F6F;
+	background-color: #3a3a3ab5;
+	border: #99b898 1px solid;
+	outline: none;
+	width: 60vw;
+	padding: 2vh 2.3vw;
+	border-radius: 5px;
+	color: #716f6f;
 }
 
 button {
-  background-color: #99B898;
-  color: #000000;
-  font-weight: 500;
-  width: 100%;
-  padding-block: 1rem;
-  border-radius: 5rem;
-  font-size: .9rem;
+	background-color: #99b898;
+	color: #000000;
+	font-weight: 500;
+	width: 100%;
+	padding-block: 1rem;
+	border-radius: 5rem;
+	font-size: 0.9rem;
 }
 </style>
