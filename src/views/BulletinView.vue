b5f4c9a3cd7da003b0adc757b10ccfc731a19f33
Luiz Felipe Nunes <106554495+LuizNunes06@users.noreply.github.com>
2024-11-02 15:09:25 +0000

    FEAT: Cração da View para boletim e sua rota

---
+++ src/views/BulletinView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DefaultBulletin from '@/components/Bulletin/DefaultBulletin.vue'
+</script>
+
+<template>
+  <main>
+    <DefaultBulletin />
+  </main>
+</template>
+
+<style scoped></style>
