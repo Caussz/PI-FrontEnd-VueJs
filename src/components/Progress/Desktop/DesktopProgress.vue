2f85fbb16257888c7d6d6808097b8cf1b63c117e
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-12-04 13:58:46 -0300

    CHORE ajuste na página de progresso / manutenção

--- src/components/Progress/Desktop/DesktopProgress.vue
+++ src/components/Progress/Desktop/DesktopProgress.vue
@@ -1,17 +1,56 @@
-<script setup></script>
+<script setup>
+import { Tools, ChevronLeft } from '@/components/icons'
+import { RouterLink } from 'vue-router'
+</script>
 <template>
   <main>
-    <h1>Em progresso</h1>
+    <RouterLink to="/">
+      <ChevronLeft />
+    </RouterLink>
+    <div>
+      <Tools size="50" />
+      <h1>Sistema em manutenção</h1>
+    </div>
   </main>
 </template>
 <style scoped>
 main {
-    display: flex;
-    justify-content: center;
-    padding: 2vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+div {
+  margin: 10vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    color: #99b898;
+  }
 }
 
 h1 {
   color: #99b898;
+}
+
+a {
+  border: 1px #505050 solid;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 4%;
+  top: 4%;
+
+  & span {
+    color: white;
+    display: flex;
+  }
 }
 </style>
