e8630ed37c31ea84d802d40e38c657860fc1a41d
IsadoraAlcantra <Isadoraalca405@gmail.com>
2024-12-16 14:37:02 -0300

    CHORE: colocando tags da table

--- src/components/Grades/Desktop/DesktopGrades.vue
+++ src/components/Grades/Desktop/DesktopGrades.vue
@@ -1,61 +1,101 @@
 <script setup></script>
 <template>
+  <div class="card">
+    <h2>Matemática</h2>
+  </div>
 
-    <div class="card">
-        <h2>Matemática</h2>
-    </div>
-
-<table>
+  <table>
     <tr>
-        <th>Trimestre 1</th>
-        <th>Recuperação</th>
-        <th>Trimestre 2</th>
-        <th>Recuperação</th>
-        <th>Trimestre 3</th>
-        <th>Recuperação</th>
+      <th>Trimestre 1</th>
+      <th>Recuperação</th>
+      <th>Trimestre 2</th>
+      <th>Recuperação</th>
+      <th>Trimestre 3</th>
+      <th>Recuperação</th>
     </tr>
     <tr>
-        <p>AP1</p>
-        <p>AP2</p>
-        <p>AP3</p>
-        <p>8</p>
-        <p>8</p>
-        <p>8</p>
+      <th>
+        <table class="inside-table">
+          <tr>
+            <th class="inside-tr">AV1</th>
+            <th class="inside-tr">AV2</th>
+            <th class="inside-tr">AV3</th>
+          </tr>
+          <tr>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+          </tr>
+        </table>
+      </th>
+      <th></th>
+      <th>
+        <table class="inside-table">
+          <tr>
+            <th class="inside-tr">AV1</th>
+            <th class="inside-tr">AV2</th>
+            <th class="inside-tr">AV3</th>
+          </tr>
+          <tr>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+          </tr>
+        </table>
+      </th>
+      <th></th>
+      <th>
+        <table class="inside-table">
+          <tr>
+            <th class="inside-tr">AV1</th>
+            <th class="inside-tr">AV2</th>
+            <th class="inside-tr">AV3</th>
+          </tr>
+          <tr>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+            <th class="inside-tr">8</th>
+          </tr>
+        </table>
+      </th>
+      <th></th>
     </tr>
-</table>
+  </table>
 </template>
 <style scoped>
 .card {
-    background-color: #2D2D2D;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: var(--white);
-}
-
-.card {
   background-color: var(--light-grey);
   height: 4vw;
-  border-radius: 10px 10px 0 0;
+  width: 70vw;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 70vw;
-    border-radius: 10px 10px 0 0;
-    margin: 2vw;
+  border-radius: 10px 10px 0 0;
+  margin: 2vw;
 
-    & h2 {
-        font-size: 1.2em;
-        padding: 1vw;
-    }
+  & h2 {
+    font-size: 1.2em;
+    padding: 1vw;
+  }
 }
 
 th {
-    border: solid 2px #2C2C2C;
-    padding: 0.7vw 1.3vw;
+  border: solid 2px #2c2c2c;
+  padding: 0.7vw 1.3vw;
 }
 
 table {
-    border-collapse: collapse;
+  border-collapse: collapse;
+}
+
+.inside-table {
+  border: solid 2px #2c2c2c;
+  color: #728971;
+  border-collapse: collapse;
+
+  & .inside-tr {
+    border: none;
+    font-weight: 400;
+  }
 }
 </style>
