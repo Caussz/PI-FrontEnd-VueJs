b69e74bc5d3c5790dabb754b83ff293f416526d2
IsadoraAlcantara <Isadoraalca405@gmail.com>
2024-12-17 10:02:20 -0300

    CHORE: ajustes no card de notas

--- src/components/Grades/Desktop/DesktopGrades.vue
+++ src/components/Grades/Desktop/DesktopGrades.vue
@@ -1,77 +1,88 @@
-<script setup></script>
-<template>
-  <div class="card">
-    <h2>Matemática</h2>
-  </div>
+<script setup>
+import { ChevronDown } from '@/components/icons'
+import { ref } from 'vue'
 
-  <table>
-    <tr>
-      <th>Trimestre 1</th>
-      <th>Recuperação</th>
-      <th>Trimestre 2</th>
-      <th>Recuperação</th>
-      <th>Trimestre 3</th>
-      <th>Recuperação</th>
-    </tr>
-    <tr>
-      <th class="father-inside-table">
-        <table class="inside-table">
-          <tr>
-            <th>AV1</th>
-            <th>AV2</th>
-            <th>AV3</th>
-          </tr>
-          <tr>
-            <th>8</th>
-            <th>8</th>
-            <th>8</th>
-          </tr>
-        </table>
-      </th>
-      <th></th>
-      <th class="father-inside-table">
-        <table class="inside-table">
-          <tr>
-            <th>AV1</th>
-            <th>AV2</th>
-            <th>AV3</th>
-          </tr>
-          <tr>
-            <th>8</th>
-            <th>8</th>
-            <th>8</th>
-          </tr>
-        </table>
-      </th>
-      <th></th>
-      <th class="father-inside-table">
-        <table class="inside-table">
-          <tr>
-            <th>AV1</th>
-            <th>AV2</th>
-            <th>AV3</th>
-          </tr>
-          <tr>
-            <th>8</th>
-            <th>8</th>
-            <th>8</th>
-          </tr>
-        </table>
-      </th>
-      <th></th>
-    </tr>
-  </table>
+const show = ref(false)
+</script>
+<template>
+  <div class="card-container">
+    <div class="card" @click="show = !show">
+      <h2>Matemática</h2>
+      <ChevronDown />
+    </div>
+    <table v-if="show">
+      <tr>
+        <th>Trimestre 1</th>
+        <th>Recuperação</th>
+        <th>Trimestre 2</th>
+        <th>Recuperação</th>
+        <th>Trimestre 3</th>
+        <th>Recuperação</th>
+      </tr>
+      <tr>
+        <th class="father-inside-table">
+          <table class="inside-table">
+            <tr>
+              <th>AV1</th>
+              <th>AV2</th>
+              <th>AV3</th>
+            </tr>
+            <tr>
+              <th>8</th>
+              <th>8</th>
+              <th>8</th>
+            </tr>
+          </table>
+        </th>
+        <th class="recuperation">9</th>
+        <th class="father-inside-table">
+          <table class="inside-table">
+            <tr>
+              <th>AV1</th>
+              <th>AV2</th>
+              <th>AV3</th>
+            </tr>
+            <tr>
+              <th>8</th>
+              <th>8</th>
+              <th>8</th>
+            </tr>
+          </table>
+        </th>
+        <th class="recuperation"></th>
+        <th class="father-inside-table">
+          <table class="inside-table">
+            <tr>
+              <th>AV1</th>
+              <th>AV2</th>
+              <th>AV3</th>
+            </tr>
+            <tr>
+              <th>8</th>
+              <th>8</th>
+              <th>8</th>
+            </tr>
+          </table>
+        </th>
+        <th class="recuperation"></th>
+      </tr>
+    </table>
+  </div>
 </template>
 <style scoped>
+.card-container {
+  margin: 2vw 0;
+}
+
 .card {
   background-color: var(--light-grey);
-  height: 4vw;
-  width: 70vw;
-  display: flex;
+  /* height: 4vw; */
+  display: grid;
+  grid-template-columns: 55% 43%;
   align-items: center;
-  justify-content: center;
+  justify-items: end;
   border-radius: 10px 10px 0 0;
-  margin: 2vw;
+  width: 70vw;
 
   & h2 {
     font-size: 1.2em;
@@ -82,6 +93,11 @@
 th {
   border: solid 2px #2c2c2c;
   padding: 0.7vw 1.3vw;
+}
+
+.recuperation {
+  color: #728971;
+  font-weight: 400;
 }
 
 table {
@@ -97,8 +113,8 @@ table {
 .inside-table {
   border: solid 2px #2c2c2c;
   border-top: 0;
-  color: #728971;
   border-collapse: collapse;
+  color: #728971;
 
   & th {
     border: none;
