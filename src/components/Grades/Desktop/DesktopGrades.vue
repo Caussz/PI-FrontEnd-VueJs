915f658c9881ecfc97c3754908b09fcfbd00540d
LuizNunes06 <Luiz.Nunes180106>
2024-12-17 13:34:14 -0300

    FIX: notas

--- src/components/Grades/Desktop/DesktopGrades.vue
+++ src/components/Grades/Desktop/DesktopGrades.vue
@@ -1,16 +1,30 @@
 <script setup>
-import { ChevronDown } from '@/components/icons'
-import { ref } from 'vue'
+import { ChevronDown } from "@/components/icons";
+import { ref } from "vue";
 
-const show = ref(false)
+const subjects = ref([
+  { subject: "BIOLOGIA", show: false },
+  { subject: "DESENVOLVIMENTO PARA DISPOSITIVOS MÓVEIS", show: false },
+  { subject: "DESENVOLVIMENTO WEB III", show: false },
+  { subject: "FILOSOFIA", show: false },
+  { subject: "FÍSICA", show: false },
+  { subject: "GEOGRAFIA", show: false },
+  { subject: "HISTÓRIA", show: false },
+  { subject: "MATEMÁTICA", show: false },
+  { subject: "PORTUGUÊS", show: false },
+  { subject: "PROJETO INTEGRADOR III", show: false },
+  { subject: "QUÍMICA", show: false },
+  { subject: "REDES DE COMPUTADORES", show: false },
+  { subject: "SOCIOLOGIA", show: false },
+]);
 </script>
 <template>
-  <div class="card-container">
-    <div class="card" @click="show = !show">
-      <h2>Matemática</h2>
+  <div class="card-container" v-for="subject in subjects" :key="subject">
+    <div class="card" @click="subject.show = !subject.show">
+      <h2>{{ subject.subject }}</h2>
       <ChevronDown />
     </div>
-    <table v-if="show">
+    <table v-if="subject.show">
       <tr>
         <th>Trimestre 1</th>
         <th>Recuperação</th>
@@ -74,18 +88,26 @@ const show = ref(false)
   margin: 2vw 0;
 }
 
+.card > table {
+  width: 75vw;
+}
+
 .card {
   background-color: var(--light-grey);
-  display: grid;
+  display: flex;
   grid-template-columns: 55% 43%;
   align-items: center;
-  justify-items: end;
+  justify-content: space-between;
   border-radius: 10px 10px 0 0;
   width: 70vw;
 
   & h2 {
     font-size: 1.2em;
     padding: 1vw;
+  }
+
+  & span {
+    margin-right: 1vw;
   }
 }
 
